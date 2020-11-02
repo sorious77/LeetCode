5214aeed2aac4baaca5b74e7bb1c1c30cb76868f
sorious77 <49060014+sorious77@users.noreply.github.com>
2020-11-02 16:59:17 +0900

    20-11-02

--- code/168.java
+++ code/168.java
@@ -1,13 +1,14 @@
-class Solution {
-    public int titleToNumber(String s) {
-        int radix = 1;
-        int index = 0;
-        
-        for(int i=s.length()-1;i>=0;i--){
-            index += (s.charAt(i) - 'A' + 1) * radix;
-            radix *= 26;
+class Solution {    
+    public String convertToTitle(int n) {
+        String answer = "";
+                
+        while(n > 0){
+            n--;
+            
+            answer = (char)(n % 26 + 'A') + answer;
+            n /= 26;
         }
         
-        return index;
+        return answer;
     }
 }
