0854b845232b84c844c7a5f3f516abf906eaa025
sorious77 <49060014+sorious77@users.noreply.github.com>
2021-01-18 13:02:28 +0900

    Update 1.java

--- code/1.java
+++ code/1.java
@@ -1,16 +1,15 @@
 class Solution {
-public
-    vectorint twoSum(vectorint& nums, int target) {
-        vectorint res;
-        for(int i=0;inums.size()-1;i++){
-            for(int j=i+1;jnums.size();j++){
+    public int[] twoSum(int[] nums, int target) {
+        int []res = new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
                 if(nums[i] + nums[j] == target){
-                    res.push_back(i);
-                    res.push_back(j);
+                    res[0] = i;
+                    res[1] = j;
                     break;
                 }
             }
-        }   
+        }
         return res;
     }
-};
+}
