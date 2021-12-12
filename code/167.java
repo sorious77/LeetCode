a70b330a31c0215d0e15d12736f8c7a436ce8869
sorious77 <49060014+sorious77@users.noreply.github.com>
2021-12-12 14:28:50 +0900

    update 21-12-12

--- code/167.java
+++ code/167.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int []answer = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
         
-        for(int i=0;i<numbers.length-1;i++){
-            for(int j=i+1;j<numbers.length;j++){
-                if(numbers[i] + numbers[j] == target){
-                    answer[0] = i+1;
-                    answer[1] = j+1;
-                    break;
-                }
+        while(true) {
+            int sum = numbers[left] + numbers[right];
+            
+            if(sum == target) {
+                return new int[] {left + 1, right + 1};
+            } else if(sum > target) {
+                right--;
+            } else {
+                left++;
             }
-        }   
-        
-        return answer;
+        }
     }
 }
