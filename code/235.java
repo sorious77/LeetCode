30c30e1d914b2956566df722a243b546e36219f2
sorious77 <sorious77@naver.com>
2022-08-22 19:10:47 +0900

    solve 22-08-22

---
+++ code/235.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(true) {
+            if(p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if(p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+    }
+}
