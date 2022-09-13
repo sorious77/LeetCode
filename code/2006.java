e4c4a324a47561529b65b1e8a9a3b99087e52dd7
sorious77 <sorious77@naver.com>
2022-09-14 00:07:21 +0900

    optimization

--- code/2006.java
+++ code/2006.java
@@ -2,10 +2,16 @@ class Solution {
     public int countKDifference(int[] nums, int k) {
         int count = 0;
         
+        Arrays.sort(nums);
+        
         for(int i = 0; i < nums.length - 1; i++) {
             for(int j = i + 1; j < nums.length; j++) {
-                if(Math.abs(nums[i] - nums[j]) == k) {
+                int diff = Math.abs(nums[i] - nums[j]);
+                
+                if(diff == k) {
                     count += 1;
+                } else if(diff > k) {
+                    break;
                 }
             }
         }
